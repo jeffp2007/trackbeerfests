4939d0af10da6feeb5d03a690ee3b2cc295a4e0a
Jeff Powers <jeffp2007@gmail.com>
2015-06-14 00:59:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trackbeerfests::Application.config.secret_key_base = '53d25559b675a7c5970d255ca67be1d7a15827b3f59dd94db36ef9295ffa35ddad1ec982e12e6827b12a51e52ab1c6a08c1918f8e78cd0e79c6ce8cee5fdc232'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trackbeerfests::Application.config.session_store :cookie_store, key: '_trackbeerfests_session'
