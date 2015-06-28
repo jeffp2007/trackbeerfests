503dccdef097f494702f11096f8bad031ca51c0e
Jeff Powers <jeffp2007@gmail.com>
2015-06-28 19:05:08 +0000

    Added capability to update events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,11 +13,25 @@ class EventsController < ApplicationController
 		redirect_to events_path
 	end
 
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		@event.update_attributes(event_params)
+		redirect_to events_path
+	end
+
 
 	private
 
 	def event_params
-		params.require(:event).permit(:event_loc_name, :event_loc_address, :event_loc_city, :event_loc_state, :event_loc_zip, :event_start_date, :event_end_date, :event_website, :event_description)
+		params.require(:event).permit(:event_name, :event_loc_name, :event_loc_address, :event_loc_city, :event_loc_state, :event_loc_zip, :event_start_date, :event_end_date, :event_website, :event_description)
 	end
 
 end
