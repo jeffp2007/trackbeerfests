0b58e1c83bc3d824ccc899e324c51805df326015
Jeff Powers <jeffp2007@gmail.com>
2015-06-28 18:37:09 +0000

    Make submit button create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,16 @@ class EventsController < ApplicationController
 		@event = Event.new
 	end
 
+	def create
+		Event.create(event_params)
+		redirect_to events_path
+	end
+
+
+	private
+
+	def event_params
+		params.require(:event).permit(:event_loc_name, :event_loc_address, :event_loc_city, :event_loc_state, :event_loc_zip, :event_start_date, :event_end_date, :event_website, :event_description)
+	end
+
 end
