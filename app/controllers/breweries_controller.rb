33720703de0b7dcd9d48d98f0d084436a708424f
Jeff Powers <jeffp2007@gmail.com>
2015-06-28 21:59:31 +0000

    Added new brewery form and create button

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -8,4 +8,16 @@ class BreweriesController < ApplicationController
 		@brewery = Brewery.new
 	end
 
+	def create
+		@brewery = Brewery.create(brewery_params)
+		redirect_to breweries_path
+	end
+
+
+
+	private
+
+	def brewery_params
+		params.require(:brewery).permit(:brewery_name, :brewery_address, :brewery_city, :brewery_state, :brewery_zip, :brewery_website, :brewery_twitter, :brewery_facebook, :brewery_description)
+	end
 end
